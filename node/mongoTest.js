a3cd8b7fe323f04e7a926ca7f747dbe5bce6ce46
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-12-27 15:00:18 -0800

    Insert and read from already created database

--- node/mongoTest.js
+++ node/mongoTest.js
@@ -2,7 +2,19 @@
 var MongoClient = require('mongodb').MongoClient;
 
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+MongoClient.connect("mongodb://localhost:27017/BMPReadout", function(err, db) {
 	if(err) throw err;
 	console.log("We are connected");
+
+	var collection = db.collection('data');
+
+	collection.find().toArray(function(err, items) {
+		console.log(items);
+	});
+
+	var doc2 = {'temperature':22.0,'pressure':101100};
+
+	//collection.insert();
+
+	collection.insert(doc2, {w:1}, function(err, result) {});
 });
