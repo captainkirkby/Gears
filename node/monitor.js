508edab0b25d8464abaf07a7189610c0c5d6cdd5
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-12-27 21:45:32 -0800

    Close database connection when we are done with it (untested on device)

--- node/monitor.js
+++ node/monitor.js
@@ -4,7 +4,7 @@
 // to read the serial port where arduino is sitting.  //
 ////////////////////////////////////////////////////////
 var serialPort = require("serialport");
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 
 
 serialPort.list(function(err, ports){
@@ -28,18 +28,18 @@ serialPort.list(function(err, ports){
 
 	var usbSerial = new serialPort.SerialPort(usbSerialPort, {
 		baudrate: 9600,
-		parser: serialPort.parsers.readline('\r\n')
+		parser: serialPort.parsers.readline("\r\n")
 	});
 
-	usbSerial.on('open',function(err) {
+	usbSerial.on("open",function(err) {
 		if(err) throw err;
-		console.log('Port open');
+		console.log("Port open");
 	});
 
 	var HEADER = "Celsius";
 	var foundHeader = false;
 
-	usbSerial.on('data', function(data){
+	usbSerial.on("data", function(data){
 		if(data.indexOf(HEADER) > -1){
 			foundHeader = true;
 			console.log("Header");
@@ -51,13 +51,18 @@ serialPort.list(function(err, ports){
 			// Connect to the db
 			MongoClient.connect("mongodb://localhost:27017/BMPReadout", function(err, db) {
 				if(err) throw err;
-				console.log("We are connected");
+				console.log("Connected");
 			
-				var collection = db.collection('data');
+				var collection = db.collection("data");
 			
 				var doc2 = {"data" : data};
 
 				collection.insert(doc2, {w:1}, function(err, result) {});
+
+				db.on("close", function(){
+					console.log("Disconnected");
+				});
+				db.close();
 			});
 
 		}
