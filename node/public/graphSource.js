2f7649f5bd7317b168fc7e1cedadb34abe6748b2
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-12-28 15:56:16 -0800

    Add implementation to stop and start real time updates

--- node/public/graphSource.js
+++ node/public/graphSource.js
@@ -31,15 +31,27 @@ $(function() {
 	}
 
 	var TIMEOUT_VALUE = 333;
+	var realTimeUpdates = false;
 
 	function continuousUpdate(){
-		displayRecentData();
-		setTimeout(continuousUpdate, TIMEOUT_VALUE);
+		if(realTimeUpdates){
+			displayRecentData();
+			setTimeout(continuousUpdate, TIMEOUT_VALUE);
+		}
 	}
 
 	$("#fetch").click(displayRecentData);
 
-	$("#continuousMode").click(continuousUpdate);
+	$("#continuousMode").click(function(){
+		if(!realTimeUpdates){
+			realTimeUpdates = true;
+			$(this).val("Stop Real Time Updates");
+		} else {
+			realTimeUpdates = false;
+			$(this).val("Start Real Time Updates");
+		}
+		continuousUpdate();
+	});
 
 	$("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
 
