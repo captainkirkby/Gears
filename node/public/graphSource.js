28d57be61565b8be21b163287d192c670098c678
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-12-28 13:35:30 -0800

    Update graph when page is loaded and DOM is ready (note: data flows the wrong way on graph (from right to left)

--- node/public/graphSource.js
+++ node/public/graphSource.js
@@ -1,6 +1,6 @@
 $(function() {
 
-	$("#fetch").click(function(){
+	function displayRecentData(){
 		var dataURL = "recent";
 
 		function onDataRecieved(data){
@@ -28,23 +28,11 @@ $(function() {
 			dataType:"json",
 			success:onDataRecieved
 		});
+	}
 
-	});
-
-	// var d1 = [];
-	// for (var i = 0; i < 14; i += 0.5) {
-	//	d1.push([i, Math.sin(i)]);
-	// }
-
-	// var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
-
-	// // A null signifies separate line segments
-
-	// var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
-
-	// $.plot("#placeholder", [ d1, d2, d3 ]);
-
-	// Add the Flot version string to the footer
+	$("#fetch").click(displayRecentData);
 
 	$("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
+
+	displayRecentData();
 });
