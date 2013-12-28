4aef5ede44722482b7aa4a5375928a0724dd06ac
David Kirkby <dkirkby@uci.edu>
2013-12-28 07:53:08 -0800

    Flesh out db+port skeleton using async framework

--- node/server.js
+++ node/server.js
@@ -4,17 +4,34 @@
 async = require('async');
 
 async.parallel({
+	// Opens a serial port connection to the TickTock module.
 	port: function(callback) {
-		setTimeout(function() {
-			callback(null,'port');
-		},200);
+		async.waterfall([
+			// Finds the tty device name of the serial port to open.
+			function(portCallback) {
+				console.log('Looking for the tty device...')
+				portCallback(null,'portName');
+			},
+			// Opens the serial port.
+			function(portName,portCallback) {
+				console.log('Opening device %s...',portName);
+				portCallback(null,'port');
+			}],
+			// Propagates our open port to the data logger.
+			function(err,port) {
+				if(!err) console.log('serial port is ready');
+				callback(err,port);
+			}
+		);
 	},
+	// Connects to the database where packets from TickTock are logged.
 	db: function(callback) {
-		setTimeout(function() {
-			callback(null,'db');
-		},400);
+		console.log('Connecting to the database...');
+		callback(null,'db');
 	}},
+	// Logs TickTock packets from the serial port into the database.
 	function(err,config) {
-		console.log(config);
+		if(err) throw err;
+		console.log('starting data logger with',config);
 	}
 );
