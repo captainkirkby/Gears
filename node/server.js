da1313615f31b0b7434eb5d05d48a26a0b6ae1cb
David Kirkby <dkirkby@uci.edu>
2013-12-29 15:29:19 -0800

    Add --no-serial and --no-database command-line options

--- node/server.js
+++ node/server.js
@@ -53,9 +53,18 @@ function assemblePacket(data,buffer,remaining,handler) {
 	return remaining;
 }
 
+// Parses command-line arguments.
+var noSerial = false;
+var noDatabase = false;
+process.argv.forEach(function(val,index,array) {
+	if(val == '--no-serial') noSerial = true;
+	else if(val == '--no-database') noDatabase = true;
+});
+
 async.parallel({
 	// Opens a serial port connection to the TickTock device.
 	port: function(callback) {
+		if(noSerial) return callback(null,null);
 		async.waterfall([
 			// Finds the tty device name of the serial port to open.
 			function(portCallback) {
@@ -100,6 +109,7 @@ async.parallel({
 	},
 	// Connects to the database where packets from TickTock are logged.
 	db: function(callback) {
+		if(noDatabase) return callback(null,null);
 		console.log('Connecting to the database...');
 		mongoose.connect('mongodb://localhost:27017/ticktock');
 		var db = mongoose.connection;
@@ -118,46 +128,55 @@ async.parallel({
 		});
 	}},
 	// Performs steps that require both an open serial port and database connection.
+	// Note that either of config.port or config.db might be null if they are disabled
+	// with command-line flags.
 	function(err,config) {
 		if(err) throw err;
-		// Logs TickTock packets from the serial port into the database.
-		console.log('starting data logger with',config);
-		var PacketModel = config.db.model;
-		// NB: packet size is hard coded here!
-		var buffer = new Buffer(12);
-		var remaining = 0;
-		config.port.on('data',function(data) {
-			console.log('received',data);
-			remaining = assemblePacket(data,buffer,remaining,function(buf) {
-				console.log('assembled',buf);
-				// Prepares packet data for storing to the database.
-				// NB: packet layout is hardcoded here!
-				var p = new PacketModel({
-					'timestamp': new Date(),
-					'temperature': buf.readInt32LE(4)/160.0,
-					'pressure': buf.readInt32LE(8)
-				});
-				console.log(p);
-				p.save(function(err,p) {
-					if(err) console.log('Error writing packet',p);
+		if(config.db && config.port) {
+			// Logs TickTock packets from the serial port into the database.
+			console.log('starting data logger with',config);
+			var PacketModel = config.db.model;
+			// NB: packet size is hard coded here!
+			var buffer = new Buffer(12);
+			var remaining = 0;
+			config.port.on('data',function(data) {
+				console.log('received',data);
+				remaining = assemblePacket(data,buffer,remaining,function(buf) {
+					console.log('assembled',buf);
+					// Prepares packet data for storing to the database.
+					// NB: packet layout is hardcoded here!
+					var p = new PacketModel({
+						'timestamp': new Date(),
+						'temperature': buf.readInt32LE(4)/160.0,
+						'pressure': buf.readInt32LE(8)
+					});
+					console.log(p);
+					p.save(function(err,p) {
+						if(err) console.log('Error writing packet',p);
+					});
 				});
 			});
-		});
+		}
 		// Defines our webapp routes.
 		var app = express();
-
+		// Serves static files from our public subdirectory.
 		app.use('/', express.static(__dirname + '/public'));
-
+		// Serves a dynamically generated information page.
 		app.get('/about', function(req, res) {
+			// TODO: flesh this out and improve handling of --no-serial or --no-database
 			res.send(util.format('tty path is %s and db is %s at %s:%d',
 				config.port.path,config.db.connection.name,config.db.connection.host,
 				config.db.connection.port));
 		});
-		app.get('/recent', function(req,res) {
-			PacketModel.find().limit(120).sort([['timestamp', -1]]).select('timestamp temperature pressure').exec(function(err,results) {
-				res.send(results);
+		if(config.db) {
+			// Serves data dynamically via AJAX.
+			var PacketModel = config.db.model;
+			app.get('/recent', function(req,res) {
+				PacketModel.find().limit(120).sort([['timestamp', -1]]).select('timestamp temperature pressure').exec(function(err,results) {
+					res.send(results);
+				});
 			});
-		});
+		}
 		// Starts our webapp.
 		console.log('starting web server on port 3000');
 		app.listen(3000);
