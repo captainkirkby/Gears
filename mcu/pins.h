4333abfc7c772cc706acf4eb2a390d3918dfd734
David Kirkby <dkirkby@uci.edu>
2014-01-03 09:40:15 -0800

    Add definitions for all I/O pins

--- mcu/pins.h
+++ mcu/pins.h
@@ -1,9 +1,24 @@
 #ifndef PINS_H
 #define PINS_H
 
-// Defines pins for the custom TickTock circuit
-#define LED_GREEN	20
-#define LED_YELLOW	21
-#define LED_RED		22
+// Defines ATMega644 pin assignments for the custom TickTock circuit
+// These are Arduino pin numbers whose mappings to the device ports
+// are specified in pins_arduino.h
+
+// LED driver outputs (low = ON)
+#define LED_GREEN		20
+#define LED_YELLOW		21
+#define LED_RED			22
+
+// ADC inputs
+#define ADC_RELHUMIDITY	30
+#define ADC_THERMISTOR	28
+#define ADC_PHOTODIODE	27
+
+// PWM outputs
+#define PWM_IR_LED		15
+
+// ICP inputs
+#define GPS_1PPS		14
 
 #endif
