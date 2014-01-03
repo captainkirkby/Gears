cb645ea0faa1c8ad4594caeb64b7bd7570b79542
David Kirkby <dkirkby@uci.edu>
2014-01-03 14:52:14 -0800

    Bug fix: ADC pins should be specified as ADC channel numbers, not their equivalent digital pin numbers

--- mcu/pins.h
+++ mcu/pins.h
@@ -2,23 +2,26 @@
 #define PINS_H
 
 // Defines ATMega644 pin assignments for the custom TickTock circuit
-// These are Arduino pin numbers whose mappings to the device ports
-// are specified in pins_arduino.h
 
 // LED driver outputs (low = ON)
+// These are Arduino pin numbers whose mappings to the device ports
+// are specified in pins_arduino.h
 #define LED_GREEN		20
 #define LED_YELLOW		21
 #define LED_RED			22
 
-// ADC inputs
-#define ADC_HUMIDITY	30
-#define ADC_THERMISTOR	28
-#define ADC_IR_IN		27
-
 // PWM outputs
+// These are Arduino pin numbers whose mappings to the device ports
+// are specified in pins_arduino.h
 #define PWM_IR_OUT		15
 
-// ICP inputs
-#define GPS_1PPS		14
+// ADC inputs
+// These are ADC channel numbers, not Arduino pin numbers.
+#define ADC_HUMIDITY	1
+#define ADC_THERMISTOR	3
+#define ADC_IR_IN		4
+
+// ICP inputs (not sure if this is needed yet)
+//#define GPS_1PPS		14
 
 #endif
