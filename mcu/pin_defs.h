d011463e907fbeee754ef00bc9999e8e89f4d322
David Kirkby <dkirkby@uci.edu>
2014-01-04 12:25:09 -0800

    Add pins defs needed for software serial

--- mcu/pin_defs.h
+++ mcu/pin_defs.h
@@ -3,4 +3,12 @@
 #define LED_DDR     DDRC
 #define LED_PORT    PORTC
 #define LED_PIN     PINC
-#define LED         PINC5
+#define LED         5
+
+/* The following UART config will only be used with SoftwareSerial */
+
+#define UART_DDR	DDRD
+#define UART_PORT	PORTD
+#define UART_PIN	PIND
+#define UART_RX_BIT	0
+#define UART_TX_BIT	1
