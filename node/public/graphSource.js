60a9a4dc1cd88718571268880fab495eb22467a2
Dylan Kirkby <dylan.kirkby@gmail.com>
2014-01-04 17:26:43 -0800

    Further generalize graphing code to turn series on and off

--- node/public/graphSource.js
+++ node/public/graphSource.js
@@ -1,5 +1,64 @@
 $(function() {
 
+	dataToPlot = {
+		"temperature" : true,
+		"pressure" : true,
+		"irLevel" : true,
+		"thermistor" : true,
+		"humidity" : true
+	};
+
+	var NUM_TEMPERATURE_SAMPLES = 11;		// o o X o o
+	var NUM_PRESSURE_SAMPLES = 55;
+	var NUM_IRLEVEL_SAMPLES = 55;
+	var NUM_THERMISTOR_SAMPLES = 55;
+	var NUM_HUMIDITY_SAMPLES = 55;
+
+
+	// labelFormatter = function(label, series){
+	// 	// return "<span class='myLegendLabel' id='"+generateLabel(label)+"'>"+label+"</span>";
+	// 	return "<span class='myLegendLabel' id='asfdjh'>"+label+"</span>";
+
+	// };
+
+	function smoothingForSet(set){
+		var lookUpTable = {
+			"temperature" : NUM_TEMPERATURE_SAMPLES,
+			"pressure" : NUM_PRESSURE_SAMPLES,
+			"irLevel" : NUM_IRLEVEL_SAMPLES,
+			"thermistor" : NUM_THERMISTOR_SAMPLES,
+			"humidity" : NUM_HUMIDITY_SAMPLES
+		};
+		var result = lookUpTable[set];
+		if(!result) result = 0;
+		return result;
+	}
+
+	function togglePlot(seriesID){
+		dataToPlot[seriesID] = !dataToPlot[seriesID];
+		displayData();		//wont display according to length
+	}
+
+	function generateLabel(name){
+		var lookUpTable = { "temperature"	: "Temperature (°C)",
+							"pressure"		: "Pressure (Pa)",
+							"irLevel"		: "IR Level (V)",
+							"thermistor"	: "Thermistor (°C)",
+							"humidity"		: "Humidity (%)" };
+		
+		var result = lookUpTable[name];
+		if(!result) {
+			$.each(lookUpTable, function(key, value){
+				if(value == name){
+					result = key;
+				} else if(key == name){
+					result = value;
+				}
+			});
+		}
+		return result;
+	}
+
 	function displayData(from, to){
 		if(from === undefined){
 			from = "DEFAULT";			// Use server default
@@ -13,25 +72,28 @@ $(function() {
 			"from" : from,
 			"to" : to
 		});
-		
-		var NUM_TEMPERATURE_SAMPLES = 11;		// o o X o o
-		var NUM_PRESSURE_SAMPLES = 55;
 
 		var POINT_SIZE = 0.5;
 
 		function onDataRecieved(data){
 			var dataSet = [];
+			var YAxesSet = [];
 
 			var count = 0;
+			var axesCount = 0;
 
-			displaySet("temperature", smoothing, NUM_TEMPERATURE_SAMPLES);
-			displaySet("pressure", smoothing, NUM_PRESSURE_SAMPLES);
-			displaySet("irLevel", smoothing, NUM_PRESSURE_SAMPLES);
-			displaySet("thermistor", smoothing, NUM_PRESSURE_SAMPLES);
-			displaySet("humidity", smoothing, NUM_PRESSURE_SAMPLES);
+			$.each(dataToPlot, function(set, display){
+				displaySet(set, smoothing, smoothingForSet(set), display);
+			})
+
+			// displaySet("temperature", smoothing, NUM_TEMPERATURE_SAMPLES);
+			// displaySet("pressure", smoothing, NUM_PRESSURE_SAMPLES);
+			// displaySet("irLevel", smoothing, NUM_IRLEVEL_SAMPLES);
+			// displaySet("thermistor", smoothing, NUM_THERMISTOR_SAMPLES);
+			// displaySet("humidity", smoothing, NUM_HUMIDITY_SAMPLES);
 
 
-			function displaySet(name, dataSmoothing, smoothingAmount){
+			function displaySet(name, dataSmoothing, smoothingAmount, visible){
 				var set = [];
 				var smoothSet = [];
 	
@@ -44,25 +106,18 @@ $(function() {
 					smoothSet.push([date, smoothPoints(name, index, smoothingAmount)]);
 				}
 
-				dataSet.push({ data: dataSmoothing ? smoothSet : set , label: generateLabel(name), yaxis: (count + 1), color : count});
+				dataSet.push({ data: dataSmoothing ? smoothSet : set , lines : { show : visible }, label: generateLabel(name), yaxis: (count + 1), color : count});
 
 				if(smoothing){
-					dataSet.push({ data: set, lines : { show : false}, points : {show : true, radius : POINT_SIZE}, yaxis: (count + 1), color : count });
+					dataSet.push({ data: set, lines : { show : false}, points : { show : visible, radius : POINT_SIZE}, yaxis: (count + 1), color : count });
 				}
 
-				count++;
-			}
+				//left or right and visible or invisible
+				var orientation = axesCount%2 ? "right" : "left";
+				YAxesSet.push({position : orientation, show : visible });
 
-			function generateLabel(name){
-				var lookUpTable = { "temperature"	: "Temperature (°C)",
-									"pressure"		: "Pressure (Pa)",
-									"irLevel"		: "IR Level (V)",
-									"thermistor"	: "Thermistor (°C)",
-									"humidity"		: "Humidity (%)" };
-				
-				var result = lookUpTable[name];
-				if(!result) result = "UNKNOWN";
-				return result;
+				count++;
+				if(visible) axesCount++;
 			}
 
 			function smoothPoints(field, index, smoothingRadius){
@@ -90,11 +145,11 @@ $(function() {
 				return sum/smoothingRadius;
 			}
 			
-			$.plot("#placeholder", dataSet, {
+			somePlot = $.plot("#placeholder", dataSet, {
 				series : { shadowSize : 0},
 				xaxes : [{ mode: "time", timezone: "browser" }],		//must include jquery.flot.time.min.js for this!
-				yaxes : [{}, { position: "right" }],
-				legend: {show : true, position : "nw" }
+				yaxes : YAxesSet,
+				legend: { show : true, position : "nw"/*, labelFormatter : labelFormatter */}
 			});
 
 		}
@@ -126,6 +181,12 @@ $(function() {
 		$("#to").prop('disabled', !enabled);
 		$("#length").prop('disabled', !enabled);
 	}
+
+	$("#placeholder").on("click", ".legendColorBox",function(){
+		$(this).css('opacity', '0.50');
+		var clickedField = generateLabel($(this).next().text());
+		togglePlot(clickedField);
+	});
 
 	$("#fetch").click(function(){
 		var from = new Date($("#from").val());
