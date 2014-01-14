28e6f7cede47fe575868f2d95d02487dc0782dd4
David Kirkby <dkirkby@uci.edu>
2014-01-13 16:31:06 -0800

    Test simple LED blinker (works!)

--- mcu/main.c
+++ mcu/main.c
@@ -1,16 +1,31 @@
-/* Name: main.c
- * Author: <insert your name here>
- * Copyright: <insert your copyright message here>
- * License: <insert your license reference here>
- */
+/***************************************************************************
+ * Top-level microcontroller firmware program.
+ * 
+ * Authors: David & Dylan Kirkby / Jan 2014.
+ ***************************************************************************/
 
 #include <avr/io.h>
+#include <util/delay.h>
+
+// bit numbers in port C
+#define LED_GREEN	5
+#define LED_YELLOW	6
+#define LED_RED		7
+
+void initLEDs() {
+	// initialize port C pins for output
+	DDRC = _BV(LED_GREEN) | _BV(LED_YELLOW) | _BV(LED_RED);
+	// turn all LEDs off (high)
+	PORTC = _BV(LED_GREEN) | _BV(LED_YELLOW) | _BV(LED_RED);
+}
 
 int main(void)
 {
-    /* insert your hardware initialization here */
+	initLEDs();
+
     for(;;){
-        /* insert your main loop code here */
+    	_delay_ms(500);
+    	PORTC ^= ~_BV(LED_YELLOW);
     }
     return 0;   /* never reached */
 }
