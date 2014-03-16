f670e2a28b4a1165f766045a8b6b3295cb69f8d1
Dylan Kirkby <dylan.kirkby@gmail.com>
2014-03-16 00:30:52 -0700

    Modify web tree to include both IR and Sensor graphs with a home page to help in the navigation

--- node/public/irSource.js
+++ node/public/irSource.js
@@ -1,58 +1,26 @@
 $(function() {
 
 	// Reverse Order
-	var seriesToPlot = ["temperature", "pressure", "irLevel", "thermistor", "humidity"];
+	var seriesToPlot = ["raw"];
 
 	// Drawing Constants
 	var NORMAL = 2;
 	var BOLD = 3;
-
-	// Recent Fetch Cache
-	var cache = {};
-	var MAX_CACHE_LENGTH = 500;
 
 	// Tick significant figures
 	var TICK_SIG_FIGS = 6;
 
 	// Plot Settings
 	var dataToPlot = {
-		"temperature" : { "visible" : true, "width" : NORMAL, "color" : 0},
-		"pressure" : { "visible" : true, "width" : NORMAL, "color" : 1},
-		"irLevel" : { "visible" : true, "width" : NORMAL, "color" : 2},
-		"thermistor" : { "visible" : true, "width" : NORMAL, "color" : 3},
-		"humidity" : { "visible" : true, "width" : NORMAL, "color" : 4}
+		"raw" : { "visible" : true, "width" : NORMAL, "color" : 0}
 	};
 
 	// Store last request parameters
-	var lastFrom;
-	var lastTo;
-
-	// Smoothing factors
-	var NUM_TEMPERATURE_SAMPLES = 11;		// o o X o o
-	var NUM_PRESSURE_SAMPLES = 55;
-	var NUM_IRLEVEL_SAMPLES = 55;
-	var NUM_THERMISTOR_SAMPLES = 55;
-	var NUM_HUMIDITY_SAMPLES = 55;
+	var lastDate;
 
 	// Real time parameters
 	var TIMEOUT_VALUE = 333;
 	var realTimeUpdates = false;
-
-	// Smoothing or not
-	var smoothing = false;
-
-	function smoothingForSet(set){
-		var lookUpTable = {
-			"temperature" : NUM_TEMPERATURE_SAMPLES,
-			"pressure" : NUM_PRESSURE_SAMPLES,
-			"irLevel" : NUM_IRLEVEL_SAMPLES,
-			"thermistor" : NUM_THERMISTOR_SAMPLES,
-			"humidity" : NUM_HUMIDITY_SAMPLES
-		};
-		var result = lookUpTable[set];
-		if(!result) result = 0;
-		return result;
-	}
 
 	function togglePlot(seriesID){
 		dataToPlot[seriesID].visible = !dataToPlot[seriesID].visible;
@@ -73,11 +41,7 @@ $(function() {
 	}
 
 	function generateLabel(name){
-		var lookUpTable = { "temperature"	: "Temperature (°C)",
-							"pressure"		: "Pressure (Pa)",
-							"irLevel"		: "IR Level (V)",
-							"thermistor"	: "Thermistor (°C)",
-							"humidity"		: "Humidity (%)" };
+		var lookUpTable = { "raw" : "IR Level"};
 		
 		var result = lookUpTable[name];
 		if(!result) {
@@ -92,27 +56,12 @@ $(function() {
 		return result;
 	}
 
-	function cacheDataPoint(date, dataPoint){
-		// Insert data point in beginning of cache, pop from end if we exceed the cache limit
-		if(cache.unshift({key : date, value : dataPoint}) > MAX_CACHE_LENGTH){
-			cache.pop();
-		}
-	}
+	function displayData(date){
+		var from;
+		var to;
 
-	function hasCachedPointForDate(date){
-		return date in cache;
-	}
-
-	// Save data in browser so if we are doing same request we don't have to re-fetch from server (caching does this maybe?)
-	function displayData(from, to){
-		// Figure out if we already have the data we need in our cache and make the modified request
-
-		if(from === undefined){
-			from = "DEFAULT";			// Use server default
-		}
-
-		if(to === undefined){
-			to = "now";
+		if(date === undefined){
+			from = "-1";			// Last data point
 		}
 
 		var dataURL = "fetch?" + $.param({
@@ -127,6 +76,7 @@ $(function() {
 		var POINT_SIZE = 0.5;
 
 		function onDataRecieved(data){
+			console.log(data[0]);
 			var dataSet = [];
 			var YAxesSet = [];
 
@@ -134,82 +84,33 @@ $(function() {
 			var axesCount = 0;
 
 			$.each(seriesToPlot, function(index, set){
-				displaySet(set, smoothing, smoothingForSet(set), dataToPlot[set].visible, dataToPlot[set].width);
+				displaySet(set, dataToPlot[set].visible, dataToPlot[set].width);
 			});
 
-			// // Cache data point if we had to retrieve it
-			// $.each(data, function(index, set){
-			// 	cacheDataPoint(data[index].timestamp, data[index]);
-			// });
-
-
-			function displaySet(name, dataSmoothing, smoothingAmount, visible, width){
+			function displaySet(name, visible, width){
 				var set = [];
-				var smoothSet = [];
 	
 				// Iterate through retrieved data
-				for(var index=0;index<data.length;index++){
+				for(var index=0;index<data[count][name].length;index++){
 					// Get timestamp as a date object
-					var date = new Date(Date.parse(data[index].timestamp));
+					var date = new Date(Date.parse(data[count].timestamp));
 
-					set.push([date, data[index][name]]);
-					smoothSet.push([date, smoothPoints(name, index, smoothingAmount)]);
+					set.push([index, data[count][name][index]]);
 				}
+				console.log(set);
 
-				dataSet.push({	data: dataSmoothing ? smoothSet : set,
+				dataSet.push({	data: set,
 								lines : { lineWidth : width, show : visible },
 								label: generateLabel(name),
 								yaxis: (count + 1),
 								color : dataToPlot[name].color});
 
-				if(smoothing){
-					dataSet.push({	data: set,
-									lines : { show : false},
-									points : { show : visible, radius : POINT_SIZE},
-									yaxis: (count + 1),
-									color : dataToPlot[name].color });
-				}
-
-				//left or right and visible or invisible
-				var orientation = axesCount%2 ? "right" : "left";
-				YAxesSet.push({	position : orientation,
-								show : visible,
-								tickFormatter : function(val, axis){ return val.toPrecision(TICK_SIG_FIGS); },
-								font : { color : width == BOLD ? "black" : "lightgrey", weight : width == BOLD ? "bold" : "normal"}});
-
 				count++;
-				if(visible) axesCount++;
-			}
-
-			function smoothPoints(field, index, smoothingRadius){
-				if(smoothingRadius < 1){
-					return data[index][field];
-				}
-
-				if(smoothingRadius%2 === 0){
-					smoothingRadius += 1;
-				}
-
-				var averageSpace = Math.floor(smoothingRadius/2);
-				var sum = 0;
-				
-				for(var i=(-1*averageSpace);i<=averageSpace;i++){
-					var t_index = i+index;
-					if(t_index<0 || t_index>=data.length){
-						//console.log("Decreasing radius");
-						return smoothPoints(field, index, smoothingRadius-2);
-					} else {
-						sum += data[t_index][field];
-					}
-				}
-
-				return sum/smoothingRadius;
 			}
 
 			somePlot = $.plot("#placeholder", dataSet, {
 				series : { shadowSize : 0},
-				xaxes : [{ mode: "time", timezone: "browser" }],		//must include jquery.flot.time.min.js for this!
-				yaxes : YAxesSet,
+				xaxes : [{ timezone: "browser" }],
 				legend: { show : true, position : "nw", sorted : "ascending"}
 			});
 
