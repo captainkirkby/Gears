b56976aa419f10091307ab9a12b5aae17300ea22
Dylan Kirkby <dylan.kirkby@gmail.com>
2014-03-24 16:32:31 -0700

    Label pins.h to correspond with schematic

--- mcu/pins.h
+++ mcu/pins.h
@@ -17,14 +17,14 @@
 
 // ADC inputs
 // These are ADC channel numbers, not Arduino pin numbers.
-#define ADC_HUMIDITY	1
+#define ADC_HUMIDITY	1			// TP2 on schematic
 #define ADC_THERMISTOR	3
 #define ADC_IR_IN		4
 
 // Test Points
-// These are ADC channel numbers, not Arduino pin numbers.
-#define TRIGGER_TEST_POINT 0
-#define PULSE_TEST_POINT 2
+// These Arduino pin numbers.
+#define TRIGGER_TEST_POINT 31		// TP3 on schematic
+#define PULSE_TEST_POINT 29			// TP1 on schematic
 
 
 // ICP inputs (not sure if this is needed yet)
