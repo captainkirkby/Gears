e8feaa4b9612b7bb30fb0ec74d683fd23864ee05
David Kirkby <dkirkby@uci.edu>
2014-03-30 17:34:22 -0700

    Basic implementation of initIR that configures pin for digital output and drives logic low for maximum IR output level

--- mcu/IR.h
+++ mcu/IR.h
@@ -1,8 +1,20 @@
 #ifndef IR_H
 #define IR_H
 
+// the port that the output pin belongs to
+#define IR_PORT		PORTD
+#define IR_DDR		DDRD
+
+// bit number for the output pin
+#define IR_PIN		7
+
 // Initialize IR output level control
 void initIR() {
+	// We will eventually allow the IR level to be modified by driving a PWM waveform
+	// from this pin (a la Arduino analogWrite) but for now we configure the port as
+	// a digital output and drive it low (which gives the maximum IR output level).
+	IR_DDR |= _BV(IR_PIN);
+	IR_PORT &= ~_BV(IR_PIN);
 }
 
 #endif
