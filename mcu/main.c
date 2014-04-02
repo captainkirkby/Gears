cc0c75a9e0a63ec272459f7adb86f25feb743388
Dylan Kirkby <dylan.kirkby@gmail.com>
2014-04-02 11:04:53 -0700

    Move free running ADC to its own file ADC.h

--- mcu/main.c
+++ mcu/main.c
@@ -14,6 +14,7 @@
 #include "IR.h"
 #include "UART.h"
 #include "TWI.h"
+#include "ACD.h"
 #include "BMP180.h"
 
 #include "packet.h"
@@ -42,6 +43,7 @@ int main(void)
     initIR();
 	initUARTs();
     initTWI();
+    initFreeRunningADC();
 
     // Initializes communication with the BMP sensor
     error = initBMP180();
@@ -50,6 +52,8 @@ int main(void)
         flashNumber(100+bootPacket.bmpSensorStatus);
         bootPacket.bmpSensorStatus = error;
     }
+
+    startFreeRunningADC();
 
     // Copies our serial number from EEPROM address 0x10 into the boot packet
     bootPacket.serialNumber = eeprom_read_dword((uint32_t*)0x10);
@@ -67,6 +71,7 @@ int main(void)
     dataPacket.type = DATA_PACKET;
     dataPacket.sequenceNumber = 0;
 
+
     while(1) {
         rippleUp();
         rippleDown();
@@ -75,8 +80,78 @@ int main(void)
         // Reads the BMP180 sensor values and saves the results in the data packet
         error = readBMP180Sensors(&dataPacket.temperature,&dataPacket.pressure);
         if(error) flashNumber(200+error);
+
+        // Store ADC run and kick off new conversion
+        if(done){
+            // Circular buffer has 800 2 byte entries
+            uint16_t rawFill = 0;
+            for(uint16_t i=currentElementIndex+1;i<CIRCULAR_BUFFER_LENGTH;++i){
+                dataPacket.raw[rawFill++] = circularbuffer[i];
+            }
+            for(uint16_t i=0;i<currentElementIndex+1;++i){
+                dataPacket.raw[rawFill++] = circularbuffer[i];
+            }
+            
+            restartFreeRunningADC();
+            // startFreeRunningADC();
+        }
+
         // Sends binary packet data
         serialWriteUSB((const uint8_t*)&dataPacket,sizeof(dataPacket));
     }
     return 0; // never actually reached
+}
+
+
+// Interrupt service routine for the ADC completion
+ISR(ADC_vect){
+    // Must read low first
+    adcValue = ADCL | (ADCH << 8);
+
+    // if(digitalRead(PULSE_TEST_POINT)){
+    //     digitalWrite(PULSE_TEST_POINT, LOW);
+    // } else {
+    //     digitalWrite(PULSE_TEST_POINT, HIGH);
+    // }
+
+    if(timer > 0){
+        //if we are still filling buffer before dump
+        ++timer;
+
+        //Time padding
+        // digitalWrite(TRIGGER_TEST_POINT, LOW);
+
+        //check if we are done filling the buffer
+        if(timer == END_TIMER){
+            LED_TOGGLE(YELLOW);
+            //uncomment this for mutiple buffers.
+            timer = 0;
+
+            //Stop the ADC by clearing ADEN
+            ADCSRA &= ~B10000000;
+
+            //Clear ADC start bit
+            ADCSRA &= ~B01000000;
+
+            //set done
+            done = 1;
+        }
+    } else {
+        // if above THRESHOLD, allow buffer to fill n number of times, dump on the serial port, then continue
+        // in either case, fill buffer once and increment pointer
+    
+        if(adcValue <= THRESHOLD){
+            //start timer
+            timer = 1;
+            // digitalWrite(TRIGGER_TEST_POINT, HIGH);
+        } else {
+            //make sure timer is stopped
+            timer = 0;
+            // digitalWrite(TRIGGER_TEST_POINT, LOW);
+        }
+    }
+
+    //Add value to buffer
+    currentElementIndex = (currentElementIndex + 1) % CIRCULAR_BUFFER_LENGTH;
+    circularbuffer[currentElementIndex] = adcValue;
 }
