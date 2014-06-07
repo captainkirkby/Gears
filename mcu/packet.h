c0ca99b25c9f600966d5f8efa929076e2da4f09a
Dylan Kirkby <dylan.kirkby@gmail.com>
2014-06-07 13:44:09 -0700

    Modify mcu side to send a larger buffer of raw IR data (enlarge from 800 to 1024 samples)

--- mcu/packet.h
+++ mcu/packet.h
@@ -6,7 +6,7 @@
 #define START_BYTE 0xFE
 #define BOOT_PACKET 0x00
 #define DATA_PACKET 0x01
-#define NUM_RAW_BYTES 1600
+#define NUM_RAW_BYTES 2048		// 1024 samples * 2 bytes per sample
 
 // Defines the boot packet structure
 typedef struct {
