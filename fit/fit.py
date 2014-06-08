49afdb13b7bb156ab284a9f747ad8d695ad32afe
David Kirkby <dkirkby@uci.edu>
2014-06-07 20:04:13 -0700

    Plot each frame

--- fit/fit.py
+++ fit/fit.py
@@ -6,6 +6,9 @@ import scipy.interpolate
 import matplotlib.pyplot as plt
 import argparse
 
+def fit(frame):
+    return 0
+
 def main():
 
     # parse command-line args
@@ -21,13 +24,23 @@ def main():
     if len(data.shape) != 1:
         print 'Data has unexpected shape',data.shape
         return -1
+
+    # initialize plot display
+    fig = plt.figure(figsize=(12,12))
+    plt.ion()
+    plt.show()
+
     # loop over data frames
+    xvec = numpy.arange(args.frame_size)
     nframe = len(data)/args.frame_size
     if len(data) % args.frame_size:
         print 'WARNING: Ignoring extra data beyond last frame'
     frames = data[:nframe*args.frame_size].reshape((nframe,args.frame_size))
     for frame in frames:
-        pass
+        plt.plot(xvec,frame,'+')
+        plt.draw()
+        params = fit(frame)
+        raw_input('hit ENTER...')
 
 if __name__ == "__main__":
     main()
