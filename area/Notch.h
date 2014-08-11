1e17fdc7390ae31eea57929d38d73ad06f5ff543
Dylan Kirkby <dylan.kirkby@gmail.com>
2014-08-11 16:53:25 -0700

    Add a center to a notch

--- area/Notch.h
+++ area/Notch.h
@@ -1,18 +1,46 @@
+/*
+Notch class to represent the notch a beam of light passes through
+Dylan Kirkby
+8/11/14
+
+Note: stores a center point, but isNotch() is called with the assumption
+that the center is at the top/center of the notch.
+*/
+
 #ifndef NOTCH_H
 #define NOTCH_H
 
 #include <cmath>
 #include "Point.h"
+#include "Polygon.h"
+
 
 // Notch Class
-class Notch
+class Notch : public Polygon
 {
 public:
-	// static const double DEFAULT_WIDTH = 0.002;	// 2mm
+
+	// Default creates at (0,0)
 	Notch(double angle)
 	{
 		this->_halfWidth = 0.002;
 		this->_angle = angle;			// In radians
+		Point point(0,0);
+		this->_center = point;
+		if(angle != 2*std::atan(1)) {	// Pi halves
+			this->_slope = std::tan(angle);
+			this->_infSlope = false;
+		} else {
+			this->_slope = 0;
+			this->_infSlope = true;
+		}
+	}
+
+	Notch(double angle, Point p)
+	{
+		this->_halfWidth = 0.002;
+		this->_angle = angle;			// In radians
+		this->_center = p;
 		if(angle != 2*std::atan(1)) {	// Pi halves
 			this->_slope = std::tan(angle);
 			this->_infSlope = false;
@@ -34,6 +62,16 @@ public:
 		return this->_angle;
 	}
 
+	double getX() const
+	{
+		return this->_center.x;
+	}
+
+	double getY() const
+	{
+		return this->_center.y;
+	}
+
 	void setAngle(double angle)
 	{
 		this->_angle = angle;
@@ -44,6 +82,7 @@ private:
 	double _angle;
 	double _slope;
 	bool _infSlope;
+	Point _center;
 };
 
 #endif
