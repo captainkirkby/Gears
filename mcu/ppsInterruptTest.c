4bd9b5b6ab72e15a3bf0d00098164b64a5be3c55
Dylan Kirkby <dylan.kirkby@gmail.com>
2014-09-01 15:47:55 -0700

    Add file to test PPS interrupt

---
+++ mcu/ppsInterruptTest.c
@@ -0,0 +1,38 @@
+#include <stdint.h>
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+
+#include "LED.h"
+#include "IR.h"
+#include "UART.h"
+#include "TWI.h"
+#include "BMP180.h"
+#include "FreeRunningADC.h"
+#include "Trimble.h"
+#include "packet.h"
+
+
+
+int main(void) {
+    // Initialize LEDs
+    initLEDs();
+
+    // Select Pin to listen to
+    PCMSK3 |= (1 << PCINT30);
+
+    // Enable Interrupts on selected pin
+    PCICR |= (1 << PCIE3);
+
+    // Enable Global Interrupts
+    sei();
+}
+
+ISR(PCI3_vect){
+    // Disable Interrupts on selected pin
+    PCICR &= ~(1 << PCIE3);
+    // Turn on the LED
+    LED_TOGGLE(GREEN);
+}
