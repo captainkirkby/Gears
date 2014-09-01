d7206f4018e34d3c9cfe2d55dad2774fe9625759
Dylan Kirkby <dylan.kirkby@gmail.com>
2014-08-31 18:11:52 -0700

    Fix the no longer mysterious escape sequence bug

--- mcu/dualUARTTest.c
+++ mcu/dualUARTTest.c
@@ -10,7 +10,7 @@
 
 #include "packet.h"
 
-#define WILD 0xBE
+#define DLE 0x10
 
 void readResponse(uint8_t numRxBytes, uint8_t *rxBytes);
 
@@ -41,16 +41,12 @@ int main(void) {
     uint8_t manualRxBytes[manualNumRxBytes];
     readResponse(manualNumRxBytes, manualRxBytes);
 
+    // serialWriteUSB((const uint8_t*)&manualRxBytes,sizeof(manualRxBytes));
+
     // Confirm response
-    uint8_t expectedManualRxBytes[] = {
-        TSIP_START_BYTE,
-        0x8F,                               // Packet Type
-        0xA5,                               // Subpacket
-        0x00,0x00,0x00,0x00,                // Echo settings
-        TSIP_STOP_BYTE1,TSIP_STOP_BYTE2
-    };
+    uint8_t expectedManualRxBytes[] = {TSIP_START_BYTE,0x8F,manualFetchPacket.packetSubType,0,0,0,0,TSIP_STOP_BYTE1,TSIP_STOP_BYTE2};
     for (int i = 0; i < manualNumRxBytes; ++i) {
-        if(expectedManualRxBytes[i] != manualRxBytes[i] && expectedManualRxBytes[i] != WILD) {      // WILD is the arbitrary wildcard
+        if(expectedManualRxBytes[i] != manualRxBytes[i] && expectedManualRxBytes[i] != 0xBE) {      // 0xBE is the arbitrary wildcard
             LED_ON(RED);
         }
     }
@@ -72,7 +68,7 @@ int main(void) {
     serialWriteGPS((const uint8_t*)&timePacket,sizeof(timePacket));
 
     // Read back 14 bytes of response
-    uint8_t commandNumRxBytes = 14;
+    uint8_t commandNumRxBytes = 15;
     uint8_t commandRxBytes[commandNumRxBytes];
     readResponse(commandNumRxBytes, commandRxBytes);
 
@@ -82,14 +78,14 @@ int main(void) {
     // Confirm response
     uint8_t expectedCommandRxBytes[] = {
         TSIP_START_BYTE,
-        0x41,                       // Packet Type
-        WILD,WILD,WILD,WILD,        // Time of week (float)
-        WILD,WILD,                  // Week number (uint16_t)       // should be 1807 (as of now...)
-        WILD,WILD,WILD,WILD,        // GPS-UTC offset (float)       // should be 16.00 ms
+        0x41,
+        0xBE,0xBE,0xBE,0xBE,        // Time of week (float)
+        0x07,0xBE,                  // Week number (uint16_t)       // should be 1807 (as of now...)
+        0x41,0x80,0x00,0x00,        // GPS-UTC offset (float)       // should be 16.00 ms
         TSIP_STOP_BYTE1,TSIP_STOP_BYTE2
     };
     for (int i = 0; i < commandNumRxBytes; ++i) {
-        if(expectedCommandRxBytes[i] != commandRxBytes[i] && expectedCommandRxBytes[i] != WILD) {     // WILD is the arbitrary wildcard
+        if(expectedCommandRxBytes[i] != commandRxBytes[i] && expectedCommandRxBytes[i] != 0xBE) {     // 0xBE is the arbitrary wildcard
             LED_ON(RED);
         }
     }
@@ -130,48 +126,38 @@ int main(void) {
         TSIP_START_BYTE,
         0x8F,                       // Packet Type
         0xAC,                       // Subtype
-        WILD,                       // Reciever Mode
-        WILD,                       // Disciplining Mode
-        WILD,                       // Self Survey Progress
-        WILD,WILD,WILD,WILD,        // Holdover Duration
-        WILD,WILD,                  // Critical Alarms
-        WILD,WILD,                  // Minor Alarms
-        WILD,                       // GPS Decoding Status
-        WILD,                       // Disciplining Activity
-        WILD,                       // Spare 1
-        WILD,                       // Spare 2
-        WILD,WILD,WILD,WILD,        // PPS Offset (float) in ns
-        WILD,WILD,WILD,WILD,        // Clock Offset (float) in ppb
-        WILD,WILD,WILD,WILD,        // DAC Value (uint32_t)
-        WILD,WILD,WILD,WILD,        // DAC Voltage (float)
-        WILD,WILD,WILD,WILD,        // Temperature (float)
-        WILD,WILD,WILD,WILD,        // Temperature (float)
-        WILD,WILD,WILD,WILD,        // Latitude (double)
-        WILD,WILD,WILD,WILD,        
-        WILD,WILD,WILD,WILD,        // Longitude (double)
-        WILD,WILD,WILD,WILD,       
-        WILD,WILD,WILD,WILD,        // Altitude (double)
-        WILD,WILD,WILD,WILD,       
-        WILD,WILD,WILD,WILD,        // PPS Quantization Error (float)
-        WILD,WILD,WILD,WILD,        // Spare 3-6
+        0xBE,                       // Reciever Mode
+        0xBE,                       // Disciplining Mode
+        0xBE,                       // Self Survey Progress
+        0xBE,0xBE,0xBE,             // Holdover Duration
+        0xBE,0xBE,                  // Critical Alarms
+        0xBE,0xBE,                  // Minor Alarms
+        0xBE,                       // GPS Decoding Status
+        0xBE,                       // Disciplining Activity
+        0xBE,                       // Spare 1
+        0xBE,                       // Spare 2
+        0xBE,0xBE,0xBE,0xBE,        // PPS Offset (float) in ns
+        0xBE,0xBE,0xBE,0xBE,        // Clock Offset (float) in ppb
+        0xBE,0xBE,0xBE,0xBE,        // DAC Value (uint32_t)
+        0xBE,0xBE,0xBE,0xBE,        // DAC Voltage (float)
+        0xBE,0xBE,0xBE,0xBE,        // Temperature (float)
+        0xBE,0xBE,0xBE,0xBE,        // Temperature (float)
+        0xBE,0xBE,0xBE,0xBE,        // Latitude (double)
+        0xBE,0xBE,0xBE,0xBE,        
+        0xBE,0xBE,0xBE,0xBE,        // Longitude (double)
+        0xBE,0xBE,0xBE,0xBE,       
+        0xBE,0xBE,0xBE,0xBE,        // Altitude (double)
+        0xBE,0xBE,0xBE,0xBE,       
+        0xBE,0xBE,0xBE,0xBE,        // PPS Quantization Error (float)
+        0xBE,0xBE,0xBE,0xBE,        // Spare 3-6
         TSIP_STOP_BYTE1,TSIP_STOP_BYTE2
     };
 
-    // Cast to struct type
-    TsipHealthResponsePacket health = *((TsipHealthResponsePacket *)healthRxBytes);
-    uint64_t latitude = health.latitude;
-    uint64_t longitude = health.longitude;
-    uint64_t altitude = health.altitude;
-
-
-    // serialWriteUSB((const uint8_t*)&latitude,sizeof(latitude));
-    // serialWriteUSB((const uint8_t*)&longitude,sizeof(longitude));
-    // serialWriteUSB((const uint8_t*)&altitude,sizeof(altitude));
-
+    serialWriteUSB((const uint8_t*)&healthRxBytes,sizeof(healthRxBytes));
 
     for (int i = 0; i < healthNumRxBytes; ++i) {
-        if(expectedHealthRxBytes[i] != healthRxBytes[i] && expectedHealthRxBytes[i] != WILD) {     // WILD is the arbitrary wildcard
-            // LED_ON(RED);
+        if(expectedHealthRxBytes[i] != healthRxBytes[i] && expectedHealthRxBytes[i] != 0xBE) {     // 0xBE is the arbitrary wildcard
+            LED_ON(RED);
         }
     }
 
@@ -184,24 +170,25 @@ void readResponse(uint8_t numRxBytes, uint8_t rxBytes[]) {
     int ch;
     uint8_t c = 0;
     uint8_t startReading = 1;
+    uint8_t escapeFlag = 0;
     // Read the given number of bytes
     while(c < numRxBytes){
         ch = getc1();
         if(ch > -1){            // Got Data
             uint8_t byte = (ch & 0xff);
-            // // Don't advance count until we see the start sequence
-            // if(byte == TSIP_START_BYTE){
-            //  startReading = 1;
-            // } 
-            // if(byte == 0x0F){
-            //     LED_ON(YELLOW);
-            // }
-            if(startReading){
-                LED_ON(GREEN);
-                // putc0(byte);
-                rxBytes[c] = byte;
-                c++;
+            if(escapeFlag){
+                escapeFlag = 0;
+                // If two DLE's in a row, only record one
+                if(byte == DLE) continue;
             }
+            // Handle special DLE character
+            if(!escapeFlag && byte == DLE) escapeFlag = 1;
+            
+            // Write to array
+            LED_ON(GREEN);
+            // putc0(byte);
+            rxBytes[c] = byte;
+            c++;
         } else if(ch == -2){    // Framing Error
             LED_ON(YELLOW);
         } else if(ch == -3){    // Data Overrun
