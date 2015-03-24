470c44e662478eec68f2d76b70ca023e5415a3fb
Dylan Kirkby <dylan.kirkby@gmail.com>
2015-03-24 13:23:20 -0700

    Increase max tries for ADC test

--- mcu/FreeRunningADC.h
+++ mcu/FreeRunningADC.h
@@ -13,7 +13,7 @@
 #define ADC_STATUS_ERROR 255
 
 // ADC test constants
-#define ADC_MAX_TEST_TRIES 1000    // This number determines how many tries the ADC gets to not be covered
+#define ADC_MAX_TEST_TRIES 10000    // This number determines how many tries the ADC gets to not be covered
 
 // ADC inputs
 // These are ADC channel numbers, not Arduino pin numbers.
